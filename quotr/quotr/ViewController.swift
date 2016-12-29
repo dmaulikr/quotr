21f9ba19fe54fef4b1c18b782c60f47fd1cd5474
Henry Ly <henryly@gmail.com>
2016-12-29 15:43:10 -0800

    Initial commit

---
+++ quotr/quotr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quotr
+//
+//  Created by Henry Ly on 12/29/16.
+//  Copyright © 2016 Henry M Ly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
